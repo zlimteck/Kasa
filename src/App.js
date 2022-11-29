2d6da1e4720f64d2c93e994ebc7b8d921be8fbd1
zlimteck <zlimteck@gmail.com>
2022-11-29 17:39:01 +0100

    Add pages and routage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Lodging from "./pages/Lodging";
+import About from "./pages/About";
+import Error from "./pages/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/lodging" element={<Lodging />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Lodging.jsx
@@ -0,0 +1,9 @@
+function Lodging() {
+    return (
+        <div>
+            <h1>Lodging</h1>
+        </div>
+    );
+}
+  
+export default Lodging;
