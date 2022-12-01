a6a4295851b7da5575e62f362a6dc8ba3356d52c
zlimteck <zlimteck@gmail.com>
2022-12-01 22:40:13 +0100

    Modify CSS Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,9 +6,9 @@ function Banner() {
     return (
         <div className="banner">
             <img className="banner_img" src={kasa_ban} alt="Banniere de l'agence Kasa"/>
-            <span className="banner_text_container">
-                <p className="banner_text">Chez vous, partout et ailleurs</p>
-            </span>
+            <div className="banner_text_container">
+                <p className="banner_text">Chez vous, <span className="underline">partout et ailleurs</span></p>
+            </div>
         </div>
     )
 }
