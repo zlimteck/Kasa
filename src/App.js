f3e46185b8146027a2a7d93b03c822027a8cfd0c
zlimteck <zlimteck@gmail.com>
2022-12-11 03:02:19 +0100

    Modify path route lodging

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/lodging" element={<Lodging />} />
+                <Route path="/lodging/:id" element={<Lodging />} />
                 <Route path="/about" element={<About />} />
                 <Route path="*" element={<Error />} />
             </Routes>
