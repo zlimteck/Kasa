759fd8a8e0b06bd3a9f2f4dfd2eab0e69b7425af
zlimteck <zlimteck@gmail.com>
2022-12-15 00:32:13 +0100

    Add img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Lodging from "./pages/Lodging";
-import About from "./pages/About";
-import Error from "./pages/Error";
+import Home from "./pages/Home/Home";
+import Lodging from "./pages/Lodging/Lodging";
+import About from "./pages/About/About";
+import Error from "./pages/Error/Error";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
