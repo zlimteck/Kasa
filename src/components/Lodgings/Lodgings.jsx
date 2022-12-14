8d2ea5bec81daa71cb883153adf42f8764953562
zlimteck <zlimteck@gmail.com>
2022-12-15 00:27:05 +0100

    Modify Code and  CSS

--- src/components/Lodgings/Lodgings.jsx
+++ src/components/Lodgings/Lodgings.jsx
@@ -1,18 +1,16 @@
 import "./Lodgings.css";
 import full_star from "../../Assets/full_star.svg";
 import empty_star from "../../Assets/empty_star.svg";
-//Boucle pour afficher les étoiles en fonction de la note
 
-
-function Lodgings ({ title, location, tags, host, rating }) {
-    let stars = [];
-    for (let i = 0; i < 5; i++) {
-        if (i < rating) {
-            stars.push(<img className="full" src={full_star} alt="Etoile de note pleine"/>);
-        } else {
-            stars.push(<img className="empty" src={empty_star} alt="Etoile de note vide"/>);
-        }
+function Lodgings ({ product, title, location, tags, host, rating }) {
+    let stars = []; // Tableau pour stocker les étoiles
+    for (let x = 0; x < rating; x++) { // Boucle pour afficher les étoiles pleines
+        stars.push(<img key={product.id + x} className="full" src={full_star} alt="Etoile de note pleine"/>);
     }
+    for (let i = stars.length; i < 5; i++) { // Boucle pour afficher les étoiles vides
+        stars.push(<img key={product.id + i} className="empty" src={empty_star} alt="Etoile de note vide"/>);
+    }
+
     return (
         <div className="infos">
             <div className="lodging_infos">
