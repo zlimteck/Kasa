9e1820fa0f0154409dfb0d97d5aed451f3bc2ac6
zlimteck <zlimteck@gmail.com>
2022-12-21 23:34:06 +0100

    Modify Error

--- src/components/Error/Error404.jsx
+++ src/components/Error/Error404.jsx
@@ -5,7 +5,7 @@ function Error404() {
     return (
         <div className="error">
             <p className='error_number'>404</p>
-            <p className='error_text'>Oups! La page que vous recherchez n'existe pas.</p>
+            <p className='error_text'>Oups! La page que vous demandez n'existe pas.</p>
             <Link to="/" className='error_link'>Retourner sur la page d'accueil</Link>
         </div>
     )
