24e9891d7b2928c28654c70f244725a3b3fc76a9
zlimteck <zlimteck@gmail.com>
2022-12-21 23:31:46 +0100

    Add About Collapse

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,22 @@
 import AboutBanner from "../../components/AboutBanner/AboutBanner";
 import "./About.css";
+import AboutInfo from "../../data/about.json";
+import Collapse from "../../components/Collapse/Collapse";
 
 function About() {
     return (
         <div className="about">
             <AboutBanner />
+            <div className="about_collapse">
+                {AboutInfo.map((info, index) => (
+                    <div key={index}>
+                    <Collapse 
+                    title={info.title}
+                    texte={info.text}
+                    />
+                </div>
+                ))}
+            </div>
         </div>
     );
 }
