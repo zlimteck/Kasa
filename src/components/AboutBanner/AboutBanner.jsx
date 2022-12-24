d343058e87c373c1bcac5893228182823bdaeca2
zlimteck <zlimteck@gmail.com>
2022-12-24 14:51:42 +0100

    Add event resize for change banner.png and modify CSS

--- src/components/AboutBanner/AboutBanner.jsx
+++ src/components/AboutBanner/AboutBanner.jsx
@@ -1,11 +1,24 @@
 import about_ban from '../../Assets/about_ban.png';
+import about_ban_mobile from '../../Assets/about_ban_mobile.png';
 import './AboutBanner.css';
+
+// Evenement pour ecouter le changement de resolution de l'ecran et changer l'image de la banniere
+function changeImage() {
+    window.addEventListener('resize', () => {
+        if (window.innerWidth < 480) {
+            document.querySelector('.aboutbanner_img').src = about_ban_mobile;
+        } else {
+            document.querySelector('.aboutbanner_img').src = about_ban;
+        }
+    })
+    return (window.innerWidth < 480 ? about_ban_mobile : about_ban); // Retourne l'image de la banniere en fonction de la resolution de l'ecran
+};
 
 // Component Banner
 function AboutBanner() {
     return (
         <div className="aboutbanner">
-            <img className="aboutbanner_img" src={about_ban} alt="Banniere de la page About du site de l'agence Kasa"/>
+            <img className="aboutbanner_img" src={changeImage()} alt="Banniere de la page A Propos du site de l'agence Kasa"/>
         </div>
     )
 }
