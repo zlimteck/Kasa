31a0e62cf64807e4d6684f5cd5b0da1821ec635a
zlimteck <zlimteck@gmail.com>
2022-12-25 02:32:29 +0100

    Change fonction for modify banner, for fix err

--- src/components/AboutBanner/AboutBanner.jsx
+++ src/components/AboutBanner/AboutBanner.jsx
@@ -1,24 +1,30 @@
 import about_ban from '../../Assets/about_ban.png';
 import about_ban_mobile from '../../Assets/about_ban_mobile.png';
+import { useState, useEffect } from 'react';
 import './AboutBanner.css';
 
-// Evenement pour ecouter le changement de resolution de l'ecran et changer l'image de la banniere
-function changeImage() {
-    window.addEventListener('resize', () => {
-        if (window.innerWidth < 480) {
-            document.querySelector('.aboutbanner_img').src = about_ban_mobile;
+// Fonction pour changer l'image en fonction de la taille de l'écran
+function useChangeImage() {
+    const [image, setImage] = useState(about_ban); // On récupère l'image de base
+    const [width, setWidth] = useState(window.innerWidth); // On récupère la largeur de l'écran
+    useEffect(() => {
+     const handleResize = () => setWidth(window.innerWidth); // On récupère la largeur de l'écran à chaque fois qu'on redimensionne la fenêtre
+        window.addEventListener('resize', handleResize); // On ajoute un écouteur d'évènement pour redimensionner la fenêtre
+        if (width < 480) {
+            setImage(about_ban_mobile);
         } else {
-            document.querySelector('.aboutbanner_img').src = about_ban;
+            setImage(about_ban);
         }
-    })
-    return (window.innerWidth < 480 ? about_ban_mobile : about_ban); // Retourne l'image de la banniere en fonction de la resolution de l'ecran
-};
+        return () => window.removeEventListener('resize', handleResize); // On supprime l'écouteur d'évènement
+    }, [width]); // Tableau de dépendances pour que l'effet quand width change
+    return image;
+}
 
 // Component Banner
 function AboutBanner() {
     return (
         <div className="aboutbanner">
-            <img className="aboutbanner_img" src={changeImage()} alt="Banniere de la page A Propos du site de l'agence Kasa"/>
+            <img className="aboutbanner_img" src={useChangeImage()} alt="Banner de la page a propos"/>
         </div>
     )
 }
