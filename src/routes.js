f8b5e0f3d3a5299497faca3fbad03d006a87a84e
zlimteck <zlimteck@gmail.com>
2023-01-07 14:28:30 +0100

    Add routes in a separate file

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ import About from "./pages/About/About";
 import Error from "./pages/Error/Error";
 
 export const RouteFile = (
-  <React.Fragment>
-    <Route path="/" element={<Home />} />
-    <Route path="/lodging/:id" element={<Lodging />} />
-    <Route path="/about" element={<About />} />
-    <Route path="*" element={<Error />} />
-  </React.Fragment>
+    <React.Fragment>
+        <Route path="/" element={<Home />} />
+        <Route path="/lodging/:id" element={<Lodging />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+    </React.Fragment>
 );
